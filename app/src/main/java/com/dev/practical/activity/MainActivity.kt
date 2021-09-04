acafcf44a5126c444c11e878cbb12de0209c69a9
pranav0116 <pranavpatel386@gmail.com>
2021-09-04 10:14:49 +0530

    First Commit

---
+++ app/src/main/java/com/dev/practical/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.practical.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.practical.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
